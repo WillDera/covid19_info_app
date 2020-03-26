2b7febab4cc6520a658b5a3de4604218e266c850
Godswill <ezeokewill@gmail.com>
2020-03-26 23:29:37 +0100

    adjusting CSS

--- src/App.js
+++ src/App.js
@@ -1,75 +1,77 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import { Card } from 'react-bootstrap';
-import './App.css';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { Card } from "react-bootstrap";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+	return (
+		<div className="App">
+			<header className="App-header">
+				<Container>
+					<div className="world">
+						<div className="wCase">
+							<Row>
+								<Col>
+									<Card className="confirmed">
+										<Card.Body>
+											<Card.Text>{10 + 10}</Card.Text>
+											<Card.Title>Total Confirmed Cases</Card.Title>
+										</Card.Body>
+									</Card>
+								</Col>
+							</Row>
+							<Row>
+								<Col>
+									<Card className="recovered">
+										<Card.Body>
+											<Card.Text>{20 * 80}</Card.Text>
+											<Card.Title>Total Recovered Cases</Card.Title>
+										</Card.Body>
+									</Card>
+								</Col>
+								{/* <Col xs lg="6">
+									<div className="chart"></div>
+								</Col> */}
+							</Row>
+							<Card className="death">
+								<Card.Body>
+									<Card.Text>{10 / 10}</Card.Text>
+									<Card.Title>Total Death Cases</Card.Title>
+								</Card.Body>
+							</Card>
+						</div>
+						<div className="wChart">
+							<Row>
+								<Col className="chart"></Col>
+							</Row>
+						</div>
+					</div>
 
-        <Container>
-          <div className="world">
-            <div>
-              <Row>
-                <Col>
-                  <Card className="confirmed" style={{ width: '18rem' }}>
-                    <Card.Body>
-                      <Card.Text>
-                        This is a wider card with supporting text below as a natural lead-in to
-                        additional content. This content is a little bit longer.
-                      </Card.Text>
-                      <Card.Title>Card title</Card.Title>
-                    </Card.Body>
-                  </Card>
-                </Col>
-              </Row>
-              <Row>
-                <Col xs={6}>
-                    <Card className="recovered" style={{ width: '18rem' }}>
-                      <Card.Body>
-                        <Card.Text>
-                          This card has supporting text below as a natural lead-in to additional
-                          content.{' '}
-                        </Card.Text>
-                      <Card.Title>Card title</Card.Title>
-                    </Card.Body>
-                  </Card>
-                </Col>
-                <Col xs lg="6">
-                  <div className="chart">
-            
-                  </div>
-                </Col>
-              </Row>
-              <Card className="death" style={{ width: '18rem' }}>
-                <Card.Body>
-                  <Card.Text>
-                    This is a wider card with supporting text below as a natural lead-in to
-                    additional content. This card has even longer content than the first to
-                    show that equal height action.
-                  </Card.Text>
-                  <Card.Title>Card title</Card.Title>
-                </Card.Body>
-              </Card>
-            </div>
-          </div>
-
-            <Card className="country">
-              <Card.Header>Featured</Card.Header>
-              <Card.Body>
-                <Card.Title>Special title treatment</Card.Title>
-                <Card.Text>
-                  With supporting text below as a natural lead-in to additional content.
-                </Card.Text>
-              </Card.Body>
-            </Card>
-        </Container>
-      </header>
-    </div>
-  );
+					<Card className="country">
+						<Card.Body>
+							<Card.Title>{"COUNTRY"}</Card.Title>
+							<Card.Text className="countryData">
+								<div className="countryConfirmed">
+									<span>45</span>
+									<span>Confirmed</span>
+								</div>
+								<div className="countryDeath">
+									<span>1</span>
+									<span>Death</span>
+								</div>
+								<div className="countryRecovered">
+									<span>2</span>
+									<span>Recovered</span>
+								</div>
+							</Card.Text>
+						</Card.Body>
+					</Card>
+				</Container>
+			</header>
+		</div>
+	);
 }
 
 export default App;
